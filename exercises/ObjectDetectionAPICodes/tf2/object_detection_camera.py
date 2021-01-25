7534476333086bf0cb06cd3db1bb28073f22abed
Alperen Çiçek <alperencicek1998@gmail.com>
2021-01-25 04:12:06 +0300

    EditionOfWebcamCode

--- exercises/ObjectDetectionAPICodes/tf2/object_detection_camera.py
+++ exercises/ObjectDetectionAPICodes/tf2/object_detection_camera.py
@@ -1,90 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-"""
-Detect Objects Using Your Webcam
-================================
-"""
-
-# %%
-# This demo will take you through the steps of running an "out-of-the-box" detection model to
-# detect objects in the video stream extracted from your camera.
-
-# %%
-# Create the data directory
-# ~~~~~~~~~~~~~~~~~~~~~~~~~
-# The snippet shown below will create the ``data`` directory where all our data will be stored. The
-# code will create a directory structure as shown bellow:
-#
-# .. code-block:: bash
-#
-#     data
-#     └── models
-#
-# where the ``models`` folder will will contain the downloaded models.
 import os
-
-DATA_DIR = os.path.join(os.getcwd(), 'data')
-MODELS_DIR = os.path.join(DATA_DIR, 'models')
-for dir in [DATA_DIR, MODELS_DIR]:
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-
-# %%
-# Download the model
-# ~~~~~~~~~~~~~~~~~~
-# The code snippet shown below is used to download the object detection model checkpoint file,
-# as well as the labels file (.pbtxt) which contains a list of strings used to add the correct
-# label to each detection (e.g. person).
-#
-# The particular detection algorithm we will use is the `SSD ResNet101 V1 FPN 640x640`. More
-# models can be found in the `TensorFlow 2 Detection Model Zoo <https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf2_detection_zoo.md>`_.
-# To use a different model you will need the URL name of the specific model. This can be done as
-# follows:
-#
-# 1. Right click on the `Model name` of the model you would like to use;
-# 2. Click on `Copy link address` to copy the download link of the model;
-# 3. Paste the link in a text editor of your choice. You should observe a link similar to ``download.tensorflow.org/models/object_detection/tf2/YYYYYYYY/XXXXXXXXX.tar.gz``;
-# 4. Copy the ``XXXXXXXXX`` part of the link and use it to replace the value of the ``MODEL_NAME`` variable in the code shown below;
-# 5. Copy the ``YYYYYYYY`` part of the link and use it to replace the value of the ``MODEL_DATE`` variable in the code shown below.
-#
-# For example, the download link for the model used below is: ``download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet101_v1_fpn_640x640_coco17_tpu-8.tar.gz``
-"""
-import tarfile
-import urllib.request
-
-# Download and extract model
-MODEL_DATE = '20200711'
-MODEL_NAME = 'ssd_resnet101_v1_fpn_640x640_coco17_tpu-8'
-MODEL_TAR_FILENAME = MODEL_NAME + '.tar.gz'
-MODELS_DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/tf2/'
-MODEL_DOWNLOAD_LINK = MODELS_DOWNLOAD_BASE + MODEL_DATE + '/' + MODEL_TAR_FILENAME
-PATH_TO_MODEL_TAR = os.path.join(MODELS_DIR, MODEL_TAR_FILENAME)
-PATH_TO_CKPT = os.path.join(MODELS_DIR, os.path.join(MODEL_NAME, 'checkpoint/'))
-PATH_TO_CFG = os.path.join(MODELS_DIR, os.path.join(MODEL_NAME, 'pipeline.config'))
-if not os.path.exists(PATH_TO_CKPT):
-    print('Downloading model. This may take a while... ', end='')
-    urllib.request.urlretrieve(MODEL_DOWNLOAD_LINK, PATH_TO_MODEL_TAR)
-    tar_file = tarfile.open(PATH_TO_MODEL_TAR)
-    tar_file.extractall(MODELS_DIR)
-    tar_file.close()
-    os.remove(PATH_TO_MODEL_TAR)
-    print('Done')
-
-# Download labels file
-LABEL_FILENAME = 'mscoco_label_map.pbtxt'
-LABELS_DOWNLOAD_BASE = \
-    'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/data/'
-PATH_TO_LABELS = os.path.join(MODELS_DIR, os.path.join(MODEL_NAME, LABEL_FILENAME))
-if not os.path.exists(PATH_TO_LABELS):
-    print('Downloading label file... ', end='')
-    urllib.request.urlretrieve(LABELS_DOWNLOAD_BASE + LABEL_FILENAME, PATH_TO_LABELS)
-    print('Done')
-"""
-# %%
-# Load the model
-# ~~~~~~~~~~~~~~
-# Next we load the downloaded model
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging
 import tensorflow as tf
 from object_detection.utils import label_map_util
@@ -100,7 +14,7 @@ for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
 # Load pipeline config and build a detection model
-configs = config_util.get_configs_from_pipeline_file("C:/Users/Alperen/Documents/Tensorflow/workspace/training_demo/exported-models/my_model")
+configs = config_util.get_configs_from_pipeline_file("C:/Users/Alperen/Documents/Tensorflow/workspace/training_demo/exported-models/my_model/pipeline.config")
 model_config = configs['model']
 detection_model = model_builder.build(model_config=model_config, is_training=False)
 
